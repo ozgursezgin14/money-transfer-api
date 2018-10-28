3c50e56a739a2d704d118e318574ee5b72438b7c
Ozgur Sezgin <ozgur.sezgin14@gmail.com>
2018-10-28 19:57:49 +0300

    Initial Commit

---
+++ src/main/java/org/example/simplemoneytransfer/MoneyTransferApiApplication.java
@@ -0,0 +1,12 @@
+package org.example.simplemoneytransfer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MoneyTransferApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MoneyTransferApiApplication.class, args);
+	}
+}
